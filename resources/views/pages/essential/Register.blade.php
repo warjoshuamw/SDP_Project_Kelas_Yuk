ccbc1d9f67a99687a428450a2c73b5f447ec99e8
AndreTheSpire <64310095+AndreTheSpire@users.noreply.github.com>
2021-09-27 18:20:45 +0700

    tambah

--- resources/views/pages/essential/Register.blade.php
+++ resources/views/pages/essential/Register.blade.php
@@ -12,11 +12,9 @@
 
 <div class="h-screen w-auto bg-gradient-to-r from-blue-400 via-purple-500 to-white-500  mx-auto flex justify-center items-center">
     <div class="justify-center flex-col flex ml-auto mr-auto items-center w-full lg:w-2/3 md:w-3/5 h-screen">
-        <form action="" class="form bg-white p-6 my-10 relative w-2/3 h-4/5 ">
-
-            <div class="icon bg-blue-600 text-white w-6 h-6 absolute flex items-center justify-center p-5" style="left:-40px"><i class="fal fa-phone-volume fa-fw text-2xl transform -rotate-45"></i></div>
-            <h3 class="text-2xl text-gray-900 font-semibold">Kelas Yuk!</h3>
-            <p class="text-gray-600"> Halaman Register</p>
+        <form action="" class="form p-6 my-10 relative w-2/3 h-4/5 ">
+            <h3 class="text-2xl font-semibold text-white">Kelas Yuk!</h3>
+            <p class="text-white"> Halaman Register</p>
             <div class="flex space-x-5 mt-3">
                 <input type="text" name="" id="Nama_depan" placeholder="Nama Depan" class="border p-2  w-1/2">
                 <input type="text" name="" id="Nama_Bekalang" placeholder="Nama Belakang" class="border p-2 w-1/2">
@@ -27,8 +25,11 @@
                 <input type="password" name="" id="Konfirmasi_Password" placeholder="Konfirmasi Password" class="border p-2 w-1/2">
             </div>
             <textarea name="" id="" cols="10" rows="3" placeholder="Ceritakan Tentangmu" class="border p-2 mt-3 w-full"></textarea>
-            <a href="/Login" class="text-base text-Blue text-right font-roboto leading-normal hover:underline mb-6">Login Saja</a>
-            <input type="submit" value="Submit" class="w-full mt-6 bg-blue-600 hover:bg-blue-500 text-white font-semibold p-3">
+            <div class="flex space-x-5 mt-3">
+            <a href="/Login" class="text-2xl text-white text-left font-roboto leading-normal hover:underline p-2  w-1/4">Login Saja</a>
+            <input type="submit" value="Submit" class="bg-blue-600 hover:bg-blue-500 text-white font-semibold border p-2  w-3/4">
+            </div >
+
 
         </form>
   </div>
