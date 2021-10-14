4446940eee404f03d0c83e4725a520e64fb4343a
AndreTheSpire <64310095+AndreTheSpire@users.noreply.github.com>
2021-10-14 20:32:10 +0700

    done for today :)

--- resources/views/pages/essential/navbarGuruDalamKelas.blade.php
+++ resources/views/pages/essential/navbarGuruDalamKelas.blade.php
@@ -33,6 +33,14 @@
         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="black">
             <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
          </svg>
+         <a href="/guru/kelas/1/kuis">
+         <p class="hover:text-gray-400  transition duration-200 ease-linear px-2" >Quiz</p>
+         </a>
+    </button>
+    <button class="flex mx-2 hidden md:flex">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="black">
+            <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
+         </svg>
          <a href="/guru/kelas/1/penilaian">
          <p class="hover:text-gray-400  transition duration-200 ease-linear px-2" >Penilaian</p>
          </a>
