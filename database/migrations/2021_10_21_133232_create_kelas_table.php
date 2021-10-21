b29bf6125cf505f0ce48b07fb92d3536845f485a
AndreTheSpire <64310095+AndreTheSpire@users.noreply.github.com>
2021-10-21 20:38:02 +0700

    dummy database

---
+++ database/migrations/2021_10_21_133232_create_kelas_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateKelasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Kelas', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('Kelas');
+    }
+}
