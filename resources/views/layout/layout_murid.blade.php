003b3a3fb43ccac58f9f281c3b66056609f70ef1
Joshua Mishael <64304163+warjoshuamw@users.noreply.github.com>
2021-11-18 19:22:18 +0700

    warna background

--- resources/views/layout/layout_murid.blade.php
+++ resources/views/layout/layout_murid.blade.php
@@ -8,7 +8,7 @@
     <title>Kelas Yuk</title>
 </head>
 <body class="" id="body">
-    <div class="bg-ocean-light dark:bg-ocean-dark dark:text-white bg-opacity-50 flex flex-col items-center ">
+    <div class="bg-ocean-light dark:bg-ocean-dark dark:text-white bg-opacity-75 flex flex-col items-center ">
         <div class="min-h-screen  flex flex-col justify-between lg:w-5/6">
             <div class="z-20">
                 @yield('navbar')
