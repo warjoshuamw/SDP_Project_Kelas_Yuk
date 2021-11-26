4661009df02b2f46d0fe509acda82c854f2d3ee2
Joshua Mishael <64304163+warjoshuamw@users.noreply.github.com>
2021-11-26 14:23:16 +0700

    fix tampilan dikit

--- resources/views/pages/essential/navbarGuruDalamKelas.blade.php
+++ resources/views/pages/essential/navbarGuruDalamKelas.blade.php
@@ -40,7 +40,7 @@
         <div id class="flex flex-col justify-center items-center hidden md:block">
             <div class="flex justify-center items-center">
                 <span class="">
-                    <svg class="h-4 w-4 text-gray-700" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <svg class="h-4 w-4 text-gray-700 dark:text-gray-200" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
                     </svg>
                 </span>
@@ -50,7 +50,7 @@
                     <div id="buttonToggler" class="bg-white w-5 h-5 rounded-full shadow-md transform" :class="{ 'translate-x-7': toggleActive}"></div>
                 </div>
                 <span class="">
-                    <svg class="h-4 w-4 text-gray-700" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <svg class="h-4 w-4 text-gray-700 dark:text-gray-200" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
                     </svg>
                 </span>
