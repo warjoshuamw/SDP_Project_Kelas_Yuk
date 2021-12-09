a69158a247009b21e7b271347afe67b7e8843a5e
Joshua Mishael <64304163+warjoshuamw@users.noreply.github.com>
2021-12-09 22:49:34 +0700

    Update guruLihatKuis.blade.php

--- resources/views/pages/guru/guruLihatKuis.blade.php
+++ resources/views/pages/guru/guruLihatKuis.blade.php
@@ -3,14 +3,14 @@
     @include('pages.essential.navbarGuruDalamKelas')
 @endsection
 @section('content')
-<div class="flex flex-row gap-2 break-words p-2 text-xs lg:text-lg flex-wrap w-full md:w-3/4 m-auto">
+<div class="flex flex-row gap-2 break-words p-2 text-xs lg:text-lg flex-wrap w-full md:w-3/4 mx-auto">
     <div class="bg-white dark:bg-ocean-light dark:bg-opacity-50 shadow-md rounded-md p-5 flex flex-col bg-opacity-75 flex-shrink w-full">
         <div class="font-semibold  border-b-2 mb-2 pb-2">
             Lihat Kuis : {{$dataKuis->kuis_judul}}
         </div>
         <div class="flex flex-row gap-2 lg:gap-4 break-normal">
             <div class="">
-                Kuis dimulai : {{$dataKuis->batas_awal}}
+                Kuis dimulai : {{date( 'd M H:i:s', strtotime($dataKuis->batas_awal) )}}
             </div>
             <div class="">
                 Kuis berakhir : {{date( 'd M H:i:s', strtotime($dataKuis->batas_akhir) )}}
